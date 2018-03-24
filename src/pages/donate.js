4345216d78e3144a96248638aa15e593665e918d
Josh Stark <jagfin1@gmail.com>
2018-03-24 12:56:59 +0000

    Added donation button

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -15,6 +15,16 @@ const DonatePage = () => (
                     <h1>Donate</h1>
                 </Col>
             </Row>
+            <Row>
+                <Col>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick" />
+                        <input type="hidden" name="hosted_button_id" value="PVYA74KK8C9D2"/>
+                        <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!" />
+                        <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
+                    </form>
+                </Col>
+            </Row>
         </Container>
         <Container className="showcase donate">
             <Row>
