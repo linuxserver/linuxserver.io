7d2855c3a3559fa243e4ece3ab573b5d3a92fed2
Alex Kretzschmar <alexktz@gmail.com>
2018-03-26 10:52:29 +0100

    Added community devs and past contributors

--- src/pages/team.js
+++ src/pages/team.js
@@ -3,13 +3,32 @@
 # Founders
 
 * <b>IronicBadger</b> - <a href="https://blog.ktz.me" target="_blank">Alex Kretzschmar</a>: Project co-founder, Red Hatter, Container Evangelist, Drone Racer and tinkerer extraordinaire. 
-* <b>j0nnymoe</b> - Jonathan Morris: insert blurb here
+* <b>j0nnymoe</b> - Project co-founder: insert blurb here
 
 # Team
 
-* <b>sparklyballs</b> - Jonathan Morris: insert blurb here
-* <b>st4rk</b> - Jonathan Morris: insert blurb here
-* <b>chbmb</b> - Jonathan Morris: insert blurb here
-* <b>j0nnymoe</b> - Jonathan Morris: insert blurb here
-* <b>j0nnymoe</b> - Jonathan Morris: insert blurb here
-* <b>j0nnymoe</b> - Jonathan Morris: insert blurb here
+* <b>sparklyballs</b> - (optional name): insert blurb here
+* <b>st4rk</b> - (optional name): insert blurb here
+* <b>chbmb</b> - (optional name): insert blurb here
+* <b>xe</b> - (optional name): insert blurb here
+* <b>j0nnymoe</b> - (optional name): insert blurb here
+
+# Community Developers
+
+Developers from the community who take an active role working with us. Want us to Dockerise your app or contribute? Join our Discord!
+
+* <b>TidusJar</b> - Ombi
+* <b>p3lim</b> - 
+* <b>imMike</b> - 
+* <b>markus101</b> - Sonarr
+* <b>TheOtherP</b> - Hydra
+* <b>Qstick</b> - Lidarr
+* <b>imadeofwaxdanny</b> - Lidarr
+* <b>Ziggimon</b> - Plex
+
+
+# Past Contributors
+
+* <b>lonix</b> - Project co-founder
+* <b>smdion</b>
+* <b>danjoi</b>
