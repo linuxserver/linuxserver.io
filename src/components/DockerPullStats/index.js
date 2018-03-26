14ad7fbd5e1689fb6de3fbefccdfc1956a1d0c71
Josh Stark <jagfin1@gmail.com>
2018-03-26 19:16:25 +0100

    Start of dynamic image count loading

--- src/components/DockerPullStats/index.js
+++ src/components/DockerPullStats/index.js
@@ -7,35 +7,65 @@ export default class DockerPullStats extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            error: null,
+            isLoaded: false,
+            images: []
+        }
+    }
+
+    componentDidMount() {
+
+        fetch('https://next.linuxserver.io/dockerhub-jsonstats/stats')
+            .then(response => response.json())
+            .then(result => {
+
+                this.setState({
+                    isLoaded: true,
+                    images: result
+                });
+            });
     }
 
     render() {
-        return (
-            <div>
-                <Row>
-                    <Col>
-                        <h3>Let the numbers speak for themselves</h3>
-                    </Col>
-                </Row>
-                <Row className="pull-stat-numbers">
-                    <Col>
-                        <div className="pull-stat-number">3</div>
-                        <div className="pull-stat-number">5</div>
-                        <div className="pull-stat-number">5</div>
-                        <div className="pull-stat-number">8</div>
-                        <div className="pull-stat-number">4</div>
-                        <div className="pull-stat-number">3</div>
-                        <div className="pull-stat-number">2</div>
-                        <div className="pull-stat-number">5</div>
-                        <div className="pull-stat-number">0</div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <p>We pride ourselves on the commitment we make to the community and our users. Over the years, we have worked hard to build a name that resonates with home server enthusiasts and newcomers. Our images are for everyone, and always will be.</p>
-                    </Col>
-                </Row>
-            </div>
-        );
+
+        const { error, isLoaded, images } = this.state;
+
+        if (isLoaded) {
+
+            return (
+                <div>
+                    <Row>
+                        <Col>
+                            <h3>Let the numbers speak for themselves</h3>
+                        </Col>
+                    </Row>
+                    <Row className="pull-stat-numbers">
+                        <Col>
+                            <div className="pull-stat-number">3</div>
+                            <div className="pull-stat-number">5</div>
+                            <div className="pull-stat-number">5</div>
+                            <div className="pull-stat-number">8</div>
+                            <div className="pull-stat-number">4</div>
+                            <div className="pull-stat-number">3</div>
+                            <div className="pull-stat-number">2</div>
+                            <div className="pull-stat-number">5</div>
+                            <div className="pull-stat-number">0</div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <p>We pride ourselves on the commitment we make to the community and our users. Over the years, we have worked hard to build a name that resonates with home server enthusiasts and newcomers. Our images are for everyone, and always will be.</p>
+                        </Col>
+                    </Row>
+                </div>
+            );
+        } else {
+
+            return (
+                <div></div>
+            )
+        }
     }
 }
