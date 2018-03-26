a22832f9cbd783ac5a92f085b338f839f1f26103
Alex Kretzschmar <alexktz@gmail.com>
2018-03-26 10:56:03 +0100

    All users

--- src/pages/team.js
+++ src/pages/team.js
@@ -7,28 +7,36 @@
 
 # Team
 
-* <b>sparklyballs</b> - (optional name): insert blurb here
-* <b>st4rk</b> - (optional name): insert blurb here
+* <b>aptalca</b> - (optional name): insert blurb here
 * <b>chbmb</b> - (optional name): insert blurb here
+* <b>kode</b> - (optional name): insert blurb here
+* <b>nicjo814</b> - (optional name): insert blurb here
+* <b>phendryx</b> - (optional name): insert blurb here
+* <b>saarg</b> - (optional name): insert blurb here
+* <b>sparklyballs</b> - (optional name): insert blurb here
+* <b>Squid</b> - (optional name): insert blurb here
+* <b>st4rk</b> - (optional name): insert blurb here
+* <b>TheLamer</b> - (optional name): insert blurb here
 * <b>xe</b> - (optional name): insert blurb here
-* <b>j0nnymoe</b> - (optional name): insert blurb here
+* <b>Zagg</b> - (optional name): insert blurb here
 
 # Community Developers
 
 Developers from the community who take an active role working with us. Want us to Dockerise your app or contribute? Join our Discord!
 
-* <b>TidusJar</b> - Ombi
-* <b>p3lim</b> - 
+* <b>hackerman</b> - 
+* <b>imadeofwaxdanny</b> - Lidarr
 * <b>imMike</b> - 
 * <b>markus101</b> - Sonarr
-* <b>TheOtherP</b> - Hydra
+* <b>p3lim</b> - 
 * <b>Qstick</b> - Lidarr
-* <b>imadeofwaxdanny</b> - Lidarr
+* <b>TheOtherP</b> - Hydra
+* <b>TidusJar</b> - Ombi
 * <b>Ziggimon</b> - Plex
 
 
 # Past Contributors
 
 * <b>lonix</b> - Project co-founder
-* <b>smdion</b>
 * <b>danjoi</b>
+* <b>smdion</b>
