c8d3c145d9c8509d7f15c5790fe7c6adc2a9cc0e
Josh Stark <jagfin1@gmail.com>
2018-03-28 20:24:51 +0100

    Very basic image list

--- src/pages/our-images.js
+++ src/pages/our-images.js
@@ -2,24 +2,23 @@ import React from 'react'
 
 import { Container, Row, Col } from 'reactstrap';
 
-import TitleAndMetaTags from '../components/TitleAndMetaTags'
+import DockerImageList from '../components/DockerImageList';
+import TitleAndMetaTags from '../components/TitleAndMetaTags';
 
-const ImagesPage = () => (
-    <div>
-        <TitleAndMetaTags title="Images built by us - LinuxServer" />
-        <Container className="our-images">
-            <Row>
-                <Col>
-                    <h1>Images built by us</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <p>Coming Soon!</p>
-                </Col>
-            </Row>
-        </Container>
-    </div>
-)
+const ImagesPage = () => (<div>
+    <TitleAndMetaTags title="Images built by us - LinuxServer"/>
+    <Container className="our-images">
+        <Row>
+            <Col>
+                <h1>Images built by us</h1>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <DockerImageList />
+            </Col>
+        </Row>
+    </Container>
+</div>)
 
 export default ImagesPage
