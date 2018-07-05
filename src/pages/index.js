3acd41b9e99c37ff0f558d302a612084689f21b4
Josh Stark <jagfin1@gmail.com>
2018-07-05 18:34:08 +0100

    A few tweaks to the team list and index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import DockerIcon from '@fortawesome/fontawesome-free-brands/faDocker'
 import JenkinsIcon from '@fortawesome/fontawesome-free-brands/faJenkins'
 import DiscordIcon from '@fortawesome/fontawesome-free-brands/faDiscord'
+import EditIcon from '@fortawesome/fontawesome-free-solid/faEdit'
 
 import TitleAndMetaTags from '../components/TitleAndMetaTags'
 
@@ -34,7 +35,7 @@ const IndexPage = () => (
                     <h3>Who are we?</h3>
                     <p>We are a group of like minded enthusiasts from across the world.</p>
                     <p>We have built and maintain the largest collection of container images on the web and at our core are the principles behind Free and Open Source Software. Our primary goal is to provide easy-to-use and streamlined Docker images with clear and concise documentation.</p>
-                    <p>We also write how-to guides and other technical tidbits over on the <a href="https://blog.linuxserver.io">Linuxserver blog</a>.
+                    <p>We also write how-to guides and other technical tidbits over on the <a href="https://blog.linuxserver.io">Linuxserver blog</a>.</p>
                 </Col>
             </Row>
         </Container>
@@ -65,22 +66,19 @@ const IndexPage = () => (
         </Container>
         <Container className="showcase">
             <Row>
-                <Col className="col" xs="12" sm="12" md="6" style={{marginBottom: '20px'}}>
-                    <img src={BlogScreenshot} alt="Blog" className="showcase-image" />
-                </Col>
                 <Col className="col" xs="12" sm="12" md="6" style={{display: 'flex'}}>
                     <div className="align-middle">
                         <h3>First class documentation!</h3>
                         <p>
-                            We often write lengthy how-to guides covering detailed topics about all the things you can do with a LinuxServer. How to guides, opinions and much more on the LinuxServer blog.
+                            We often write lengthy how-to guides covering detailed topics about all the things you can do with our containers including How-To guides, opinions and much more on the <a href="https://blog.linuxserver.io" target="_blank">LinuxServer blog</a>.
                         </p>
                         <p>
-                            To add to this we've begun work on an extensive documentation section for getting started with consuming our images, contributing to the site and lots more.
-                        </p>
-                        <p>
-                            <a href="https://blog.linuxserver.io" target="_blank"><FontAwesomeIcon icon={edit} style={{color: '#7289DA', fontSize: '5rem'}} /></a>
+                            To add to this we've begun work on an <Link to="/docs">extensive documentation section</Link> for getting started with consuming our images, contributing to the site and lots more.
                         </p>
                     </div>
+                </Col>
+                <Col className="col" xs="12" sm="12" md="6" style={{marginBottom: '20px'}}>
+                    <img src={BlogScreenshot} alt="Blog" className="showcase-image" style={{width: '80%'}} />
                 </Col>
             </Row>
         </Container>
