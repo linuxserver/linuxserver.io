79588e3f9e2c6be758bd93d500d1c38e11dc78c1
Josh Stark <JAGFin1@users.noreply.github.com>
2018-07-07 19:22:32 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
         },
         {
             resolve: 'gatsby-plugin-sitemap',
-            'options': {
+            options: {
                 serialize: ({ site, allSitePage }) =>
                     allSitePage.edges.map(edge => {
                         return {
